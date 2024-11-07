4353586e83072a8ef4c5bf1a34af0bd0f28b81c7
Flynn <tomasz26bojko@gmail.com>
2024-11-07 22:19:31 +0100

    6.2 Zadanie, pierwszy test jednostkowy. Test dla klasy Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,48 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
         System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // tu umieść kolejny test ->
+        System.out.println("Test - for *Calculator* class:");
+
+        Calculator calculatorTest = new Calculator(3,1);
+
+        int argumentA = calculatorTest.getA();
+        int argumentB = calculatorTest.getB();
+
+        if (argumentA==3 && argumentB==1) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("compering arguments error!");
+        }
+
+        int addResult = calculatorTest.add(3, 1);
+        int subtractResult = calculatorTest.subtract(3, 1);
+
+        if (addResult==4) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Add method error");
+        }
+        if (subtractResult==2) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Subtract method error");
+        }
     }
 }
