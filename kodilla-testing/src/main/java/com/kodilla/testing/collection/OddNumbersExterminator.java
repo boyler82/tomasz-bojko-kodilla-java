9c9b683e00dadd43bccf7bfe293c6252c86e1759
Flynn <tomasz26bojko@gmail.com>
2024-11-08 12:56:09 +0100

    Zadanie 6.3. Usunołem pola i konstruktory

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -5,16 +5,6 @@ import java.util.List;
 
 public class OddNumbersExterminator {
 
-    private List<Integer> providedList;
-
-    public OddNumbersExterminator(List<Integer> providedNumbers) {
-        this.providedList = providedNumbers;
-    }
-
-    public List<Integer> getProvidedList() {
-        return providedList;
-    }
-
     public List<Integer> exterminate(List<Integer> numbers) {
         List<Integer> evenNumbersList = new ArrayList<>();
         for (int number : numbers) {
