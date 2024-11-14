9bb72ae00d474397ba7fb288bdb53e72434a4982
Flynn <tomasz26bojko@gmail.com>
2024-11-14 11:15:20 +0100

    Zadanie 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser2.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser2.java
@@ -1,18 +1,16 @@
 package com.kodilla.stream.immutable;
 
-public final class ForumUser {
+public final class ForumUser2 {
     private final String userName;
     private final String realName;
 
-    public ForumUser(final String userName, final String realName) {
+    public ForumUser2(final String userName, final String realName) {
         this.userName = userName;
         this.realName = realName;
     }
-
     public String getUserName() {
         return userName;
     }
-
     public String getRealName() {
         return realName;
     }
